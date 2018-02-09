4ff69a3841b3ee3771c15d0bf632808dd0b861a0
hmtanbir <tanbir2025@gmail.com>
2018-02-09 21:43:43 +0600

    video news model is done

---
+++ video/models.py
@@ -0,0 +1,19 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Video(models.Model):
+    """
+    Description: Video news
+    """
+    ts = models.DateTimeField(auto_now=True)
+    image_file = models.ImageField(max_length=255)
+    image_click_url = models.CharField(max_length=255)
+	    
+    
+    def __unicode__(self):
+    	return self.image_click_url
+
+    def __str__(self):
+    	return self.image_click_url	
