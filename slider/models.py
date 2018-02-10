6185492154f5139aff428df5c0b867aba4ec4fe4
hmtanbir <tanbir2025@gmail.com>
2018-02-10 12:26:13 +0600

    slider add

---
+++ slider/models.py
@@ -0,0 +1,24 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Slider(models.Model):
+    """
+    Description: Video news
+    """
+    ts = models.DateTimeField(auto_now=True)
+    image_file = models.ImageField(max_length=255)
+    caption = models.CharField(max_length=255)
+    
+
+    class Meta:
+    	verbose_name = "Slider Picture"
+    	verbose_name_plural = "Slider Pictures"
+	    
+    
+    def __unicode__(self):
+    	return self.caption
+
+    def __str__(self):
+    	return self.caption
